60b5ebe1bac7f79078f9043219f7fd8c02dedece
Ryan Caille <texasryan82@gmail.com>
2017-02-26 15:12:57 -0600

    initial webapi setup

---
+++ TooManyRules.WebApi/Controllers/RulesController.cs
@@ -0,0 +1,58 @@
+﻿// /********************************************************************************************************************************
+// * Copyright 2017 Ryan Caille
+// * 
+// * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+// * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+// * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+// * is furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// * 
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+// * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ********************************************************************************************************************************/
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TooManyRules.Models;
+
+namespace TooManyRules.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class RulesController : Controller
+    {
+        // GET api/rules
+        [HttpGet]
+        public IEnumerable<Rule> Get()
+        {
+            return new[] {new Rule{Id = 1} };
+        }
+
+        // GET api/rules/5
+        [HttpGet("{id}")]
+        public Rule Get(int id)
+        {
+            return new Rule {Id = 1};
+        }
+
+        // POST api/rules
+        [HttpPost]
+        public void Post([FromBody] Rule value)
+        {
+        }
+
+        // PUT api/rules/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Rule value)
+        {
+        }
+
+        // DELETE api/rules/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TooManyRules.WebApi/Startup.cs
@@ -0,0 +1,55 @@
+﻿// /********************************************************************************************************************************
+// * Copyright 2017 Ryan Caille
+// * 
+// * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+// * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+// * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+// * is furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// * 
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+// * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ********************************************************************************************************************************/
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TooManyRules.WebApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
